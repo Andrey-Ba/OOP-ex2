3e8162769b6a22e33540b66ff8503dca147636d7
Andrey <andreyba2000@gmail.com>
2020-12-04 10:12:53 +0200

    Added testing folder

---
+++ Tests/api/DWGraph_DSTest.java
@@ -0,0 +1,15 @@
+package api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DWGraph_DSTest {
+
+    @Test
+    void basictest()
+    {
+        assertEquals(1,1);
+    }
+
+}
